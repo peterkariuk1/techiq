0aa1d8b459fecd54cd6cf650004ec47ddd4fbcac
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-04-20 02:10:08 +0300

    style: add nav component responsiveness

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import logoImage from "../assets/lorislogo.png";
+import searchIcon from "../assets/search.png";
+import cartIcon from "../assets/cart.png";
+import profileIcon from "../assets/user.png";
+import "../styles/nav.css";
+
+const Header = () => {
+  return (
+    <nav>
+      <img className='loris-logo' src={logoImage} />
+      <div className="searchbox">
+        <input type="text" placeholder="Search by category" />
+        <div className="search-button-container">
+          <img src={searchIcon} />
+        </div>
+      </div>
+      <div className="right">
+        <div className="profile-container">
+          <img src={profileIcon} />
+        </div>
+        <div className="cart-container">
+          <img src={cartIcon} />
+          <div className="cart-counter-container">
+            <p>3</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
