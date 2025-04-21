a490ec533a04e5944a604afad124cd6d0ac6fd13
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-04-21 23:40:47 +0300

    feat: add mockup admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,3 +1,5 @@
+import '../styles/admin.css';
+
 const Admin = () => {
   return (
     <div className="admin-page">
@@ -9,25 +11,67 @@ const Admin = () => {
       </div>
       <div className="add-products">
         <p>Add Image</p>
-        <input type="file" /> 
-        
+        <input type="file" />
+
         <p>Add Price of the product</p>
-        <input type="number" /> 
-        
+        <input type="number" />
+
         <p>Specify if item is currently stocked</p>
         <label>
-                <input 
-                  type="radio" 
-                /> In Stock
-              </label>
-              <label>
-                <input 
-                  type="radio"  
-                /> Out of Stock
-              </label>        
-        
+          <input type="radio" /> In Stock
+        </label>
+        <label>
+          <input type="radio" /> Out of Stock
+        </label>
+
         <p>Add the item's category</p>
-        
+        <div className="scent-categories-radio-buttons">
+          <label>
+            <input type="radio" />
+            Auto Perfumes
+          </label>
+          <label>
+            <input type="radio" />
+            Body Care
+          </label>
+          <label>
+            <input type="radio" />
+            Cologne
+          </label><label>
+            <input type="radio" />
+            Diffusers
+          </label><label>
+            <input type="radio" />
+            Dmar
+          </label><label>
+            <input type="radio" />
+            Frequence
+          </label><label>
+            <input type="radio" />
+            Loris Perfumes
+          </label><label>
+            <input type="radio" />
+            Lotion
+          </label><label>
+            <input type="radio" />
+            Mist
+          </label><label>
+            <input type="radio" />
+            Niche Perfumes
+          </label><label>
+            <input type="radio" />
+            Niche Diffusers
+          </label><label>
+            <input type="radio" />
+            Room Spray
+          </label><label>
+            <input type="radio" />
+            Mystery Perfume
+          </label><label>
+            <input type="radio" />
+            Others
+          </label>
+        </div>
       </div>
     </div>
   );
