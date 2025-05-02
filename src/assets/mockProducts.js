ef763494fb567ddcdc182cea63c631d25553b92c
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-05-02 17:18:28 +0300

    feat: add mock data for mini grids

---
+++ src/assets/mockProducts.js
@@ -0,0 +1,594 @@
+import imageMen from "../assets/loriscologne.jpg";
+import imageLadies from "../assets/lorisfrequence.png";
+import imageNiche from "../assets/lorisniche.jpeg";
+import imageBestSeller from "../assets/lorisdiffusers.jpg";
+
+const products = [{
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+}, {
+  id: 12,
+  name: 'Men1B',
+  quantities: 100,
+  price: 1200,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'men',
+  category: 'niche',
+  image: imageMen,
+  description: `This is a description for a product`,
+
+}, {
+  id: 13,
+  name: 'Ladies1N',
+  quantities: 50,
+  price: 1500,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'ladies',
+  category: 'cologne',
+  image: imageLadies,
+  description: `This is a description for a product`,
+}, {
+  id: 14,
+  name: 'Uni1B',
+  quantities: 100,
+  price: 1250,
+  inStock: true,
+  isBestSeller: true, // every 5th item
+  gender: 'unisex',
+  category: 'niche',
+  image: imageBestSeller,
+  description: `This is a description for a product`,
+}, {
+  id: 15,
+  name: 'Men2N',
+  quantities: 100,
+  price: 900,
+  inStock: true,
+  isBestSeller: false, // every 5th item
+  gender: 'men',
+  category: 'body spray',
+  image: imageMen,
+  description: `This is a description for a product`,
+},]
+
+
+export default products;
