f81025056cef9e4f975e069ef1a037e93c2edda0
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-05-07 03:12:56 +0300

    perf: improve seo optimization using react-helmet library

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "../components/App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <HelmetProvider>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </HelmetProvider>
 );
